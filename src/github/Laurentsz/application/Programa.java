59d1ddd89839229b83f3ceab2b2770a907bbb79b
Rai Laurentino <rai.laurentino@digisystem.com.br>
2021-06-23 13:39:59 -0300

    Feature (Posição) - Adicionando classe Posicao
    Primeiro commit.

---
+++ src/github/Laurentsz/application/Programa.java
@@ -0,0 +1,13 @@
+package github.Laurentsz.application;
+
+import github.Laurentsz.boardgame.Posicao;
+
+public class Programa {
+
+    public static void main(String[] args) {
+
+        Posicao pos1 = new Posicao(3, 5);
+
+        System.out.println(pos1.toString());
+    }
+}

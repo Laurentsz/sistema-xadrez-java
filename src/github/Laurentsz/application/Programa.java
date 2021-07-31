f58a3e5be8f9f47a4e26e91a8a9f2ccb2dd3b0f5
Rai Laurentino <rai.laurentino@digisystem.com.br>
2021-07-31 18:09:47 -0300

    Feature(board) Criação do tabuleiro
    Criação das classes de Peça e Tabuleiro

--- src/github/Laurentsz/application/Programa.java
+++ src/github/Laurentsz/application/Programa.java
@@ -1,13 +1,12 @@
 package github.Laurentsz.application;
 
 import github.Laurentsz.boardgame.Posicao;
+import github.Laurentsz.boardgame.Tabuleiro;
 
 public class Programa {
 
     public static void main(String[] args) {
 
-        Posicao pos1 = new Posicao(3, 5);
-
-        System.out.println(pos1.toString());
+        Tabuleiro tabuleiro = new Tabuleiro(8,8);
     }
 }

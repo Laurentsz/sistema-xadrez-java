47751fbd089c0fe57decfd49d3e059a9aecee87d
Rai Laurentino <rai.laurentino@digisystem.com.br>
2021-09-02 21:39:35 -0300

    feat(development) Clearing Screen and Printing exceptions

--- src/github/Laurentsz/application/Program.java
+++ src/github/Laurentsz/application/Program.java
@@ -1,10 +1,12 @@
 package github.Laurentsz.application;
 
 import github.Laurentsz.boardgame.Board;
+import github.Laurentsz.chess.ChessException;
 import github.Laurentsz.chess.ChessMatch;
 import github.Laurentsz.chess.ChessPiece;
 import github.Laurentsz.chess.ChessPosition;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
@@ -15,15 +17,23 @@ public class Program {
         ChessMatch chessMatch = new ChessMatch();
 
         while (true){
-            UI.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.print("Source: ");
-            ChessPosition source = UI.readChessPosition(sc);
-            System.out.println();
-            System.out.print("Target: ");
-            ChessPosition target = UI.readChessPosition(sc);
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-
+            try{
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            }catch (ChessException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }catch (InputMismatchException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
         }
 
 
